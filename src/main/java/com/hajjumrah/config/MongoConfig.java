d8ec9fbef3bfb778e59c2f462a4cbfb64d08996b
mongodbdummy689 <mongodbdummy689@gmail.com>
2025-05-09 15:35:24 +0530

    =Config changes

--- src/main/java/com/hajjumrah/config/MongoConfig.java
+++ src/main/java/com/hajjumrah/config/MongoConfig.java
@@ -2,12 +2,17 @@ package com.hajjumrah.config;
 
 import com.hajjumrah.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.index.IndexOperations;
 import org.springframework.data.mongodb.core.index.IndexResolver;
 import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import jakarta.annotation.PostConstruct;
 
@@ -20,11 +25,31 @@ public class MongoConfig {
     @Autowired
     private MongoMappingContext mongoMappingContext;
 
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoConverter converter) {
+        return new MongoTemplate(mongoDbFactory, converter);
+    }
+
     @PostConstruct
     public void initIndices() {
-        // Create indexes for User collection
-        IndexOperations indexOps = mongoTemplate.indexOps(User.class);
-        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-        resolver.resolveIndexFor(User.class).forEach(indexOps::ensureIndex);
+        try {
+            // Create indexes for User collection
+            IndexOperations indexOps = mongoTemplate.indexOps(User.class);
+            IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
+            resolver.resolveIndexFor(User.class).forEach(indexOps::ensureIndex);
+        } catch (Exception e) {
+            // Log the error but don't fail the application startup
+            System.err.println("Error creating MongoDB indices: " + e.getMessage());
+        }
     }
 } 
